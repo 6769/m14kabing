fd3db063d34dc4bfa27c68be03b7c6e7f9656009
5pipi <5pipitk@gmail.com>
2016-06-03 21:28:48 +0800

    invert :small improve

--- Python/ImageInvert/BatchImageToreverseColor.py
+++ Python/ImageInvert/BatchImageToreverseColor.py
@@ -9,7 +9,19 @@ from PIL import Image,ImageChops
 import os
 import logging
 import sys
+
+pause_mark=False
 mark_of_new='.Inverted.'
+help_string="""
+
+PIL invert 0.2 简单的反色工具
+用法： 应用程序.exe  File...
+       或者直接将图片拖曳到exe图标上
+举例： BatchImageToreverseColor.exe testpic0.png testpic1.png testpic2.png...
+
+
+"""
+
 logging.basicConfig(level=logging.INFO)
 def picReverse(addr):
     logging.info('[%s]start To solve'%addr)
@@ -18,6 +30,8 @@ def picReverse(addr):
     except OSError as e:
         print(e)
         logging.info(e)
+        logging.info('------------Failed,file didn\'t open----------')
+        pause_mark=True
         return e
     picformat=currntpic.format
     newpic=ImageChops.invert(currntpic)
@@ -26,18 +40,28 @@ def picReverse(addr):
     logging.info('[%s]start To save the inverted pic'%newfilename)
     try:
         newpic.save(newfilename)
-        
+        logging.info('------------Success,file saved------------')
     except :
         logging.info('[%s] save Error'%newfilename)
         logging.info('********Please give Author a feedback********')
-    logging.info('------------Success,file saved------------')
+        pause_mark=True
+    
 def main(argv=None):
     if(argv==None):
         piclist=sys.argv[1:]
     else:
         piclist=argv
+        
+        
+    if(piclist==[]):
+        logging.info(help_string)
+        os.system('pause')
+        sys.exit(0)
+        
     for i in piclist:
         picReverse(i)
+    if(pause_mark):
+        os.system('pause')
         
         
 if __name__=='__main__':
@@ -46,8 +70,5 @@ if __name__=='__main__':
         main(['testpic.png',])
     else:
         main()
-    
-    
-    
-    
+
     
